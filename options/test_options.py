91ab84e35b33a0289c95f6fccc51c698475085ea
NomotoEriko <nomoto.eriko@ist.osaka-u.ac.jp>
2019-07-29 13:52:13 +0900

    :sparkles: sample data randomely

--- options/test_options.py
+++ options/test_options.py
@@ -13,11 +13,14 @@ class TestOptions(BaseOptions):
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
         parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+        parser.add_argument('--random_sample', dest="serial_batches", action='store_false',
+                            help='results on randomly chosen images')
         # Dropout and Batchnorm has different behavioir during training and test.
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
         parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
         # rewrite devalue values
         parser.set_defaults(model='test')
+        parser.set_defaults(serial_batches=True)
         # To avoid cropping, the load_size should be the same as crop_size
         parser.set_defaults(load_size=parser.get_default('crop_size'))
         self.isTrain = False
